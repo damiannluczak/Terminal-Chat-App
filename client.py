02426581d4e937f835eadf1757ccf3a9b2c95962
damiannluczak <152283021+damiannluczak@users.noreply.github.com>
2025-04-03 23:41:53 +0200

    clean output /list /help added

--- client.py
+++ client.py
@@ -1,49 +1,69 @@
 import socket
 import threading
-import receive
 
-#seting header to 64 b
+
 HEADER = 64
-#specyfing port i ip
-PORT =5050
+
+PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 SERVER = "127.0.0.1"
 
-ADDR = (SERVER,PORT)
-
+ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 username = input("Enter Your Username")
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
 
 def receive():
     while True:
         try:
             msg = client.recv(2048).decode(FORMAT)
-            print(msg)
+            if msg.startswith(f"[{username}]"):
+                print(f"\r🟢 {msg}\n> ", end="")
+            else:
+                print(f"\r{msg}\n> ", end="")
         except:
             print("ERROR Connection closed or lost.")
             break
 
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+
 def send(msg):
-    message = f"[{username}]: {msg}".encode(FORMAT)
+    msg = msg.strip()
+
+    if msg.startswith("/"):
+        message = msg.encode(FORMAT)
+    else:
+        message = msg.encode(FORMAT)
+
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
 
-    send_length +=b' ' * (HEADER- len(send_length))
     client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+
 
 while True:
-    msg = input()
+    msg = input("> ")
+
+    if msg == "/help":
+        print("Dostępne komendy:\n/help\n/list\n!DISCONNECT")
+        continue
+
+    if msg == "/list":
+        send("/list")
+        continue
+
     if msg == DISCONNECT_MESSAGE:
         send(msg)
         break
+
     send(msg)
--- server.py
+++ server.py
@@ -1,44 +1,69 @@
 import socket
 import threading
+from datetime import datetime
 
-# seting header to 64 b
+
 HEADER = 64
 
-# specyfing port i ip
+
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-# creasting a socket INTET == using ipv4   stream - we will stream data
+# creasting a socket INTET ==  ipv4
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(ADDR)
 
-clients = []
+clients = {}  # conn : ussername
 
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION {addr} connected.")
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    # odbieranie nazwy użytkownika
+    username_length = conn.recv(HEADER).decode(FORMAT)
+    if username_length:
+        username_length = int(username_length)
+        username = conn.recv(username_length).decode(FORMAT)
+        clients[conn] = username
+
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+
+            # obsługa  /list
+            if msg == "/list":
+                user_list = "\n".join(clients.values())
+                conn.send(f"📜 Online users:\n{user_list}".encode(FORMAT))
+                continue
+
+            # obsługa dc
             if msg == DISCONNECT_MESSAGE:
                 connected = False
-            print(msg)
+                break
+            if msg.startswith("/"):
+                continue
+
+            print(f"[{username}] {msg}")
+
+            # rozsylanie
             for client in clients:
                 if client != conn:
                     try:
-                        client.send(msg.encode(FORMAT))
+                        client.send(f"[{username}]: {msg}".encode(FORMAT))
                     except:
                         client.close()
-                        clients.remove(client)
+                        del clients[client]
+
     conn.close()
     if conn in clients:
-        clients.remove(conn)
+        del clients[conn]
 
 
 def start():
@@ -46,10 +71,9 @@ def start():
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
-        clients.append(conn)
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS]{threading.activeCount()}")
+        print(f"[ACTIVE CONNECTIONS]{threading.active_count()}")
 
 
 print("[START] server is starting")
